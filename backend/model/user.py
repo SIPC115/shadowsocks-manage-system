2c888defef79aa094c1ba5fd833374e52fd727b9
ruoyang.zry <ruoyang.zry@alipay.com>
2017-05-26 14:14:30 +0800

    add login/signup

---
+++ backend/model/user.py
@@ -0,0 +1,29 @@
+from run import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(64))
+    # 加密密码
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+    # 验证密码
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+    # 生成token，并设置过期时间
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+    # 静态的验证token的方法
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # token过期
+        except BadSignature:
+            return None    # token无效
+        user = User.query.get(data['id'])
+        return user
--- backend/run.py
+++ backend/run.py
@@ -1,11 +1,48 @@
-from flask import Flask, request, jsonify
+# coding:utf-8
 import json
+import os
+
+from flask import Flask, abort, request, jsonify, g, url_for, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+
+from user impoer *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://sipc115:sipc115@127.0.0.1:3306/shadowsocks_manage'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+
+db = SQLAlchemy(app)
+
+auth = HTTPBasicAuth()
 
 @app.route('/index')
 def _index():
     return 'hello flask'
+
+@app.route('/signup', methods=['POST'])
+def _signup():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    # 成功注册后返回用户名，Location后面接着的是跳转的地址
+    return jsonify({'username': user.username})
+
+@app.route('/login')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    resp = make_response(jsonify({'token': token.decode('ascii'), 'duration': 600}))
+    resp.set_cookie('user', token)
+    return resp
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
